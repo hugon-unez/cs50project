ae9d6f9cb5fe1e990d312757bc6a14a0b06940bc
hugon-unez <63176618+hugon-unez@users.noreply.github.com>
2022-12-06 14:18:06 -0500

    Add files via upload

---
+++ Twittrak/app.py
@@ -0,0 +1,60 @@
+# reused general implementation from finance in order to have a running front end
+import os
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+from backend.stock_data import recent_price
+from backend.stock_prediction import predicted_change
+
+# short helper to format money from finance pset
+def usd(value):
+    return f"${value:,.2f}"
+
+# Configure application
+app = Flask(__name__)
+app.jinja_env.filters["usd"] = usd
+
+
+# Configure session to use filesystem (instead of signed cookies)
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/correlate", methods=['GET','POST'])
+def correlate():
+    if request.method == "POST":
+        username = request.form.get("username")
+        #check if valid username
+        change = predicted_change(username)
+        if change == "Invalid Input":
+            return redirect("/")
+        new_price = recent_price()[0] + change
+        #input to tweet thing
+        #interpret result and show
+        if change > 0:
+            return render_template("result.html", username = username, change = change, new_price=new_price)
+        if change < 0:
+            change = abs(change)
+            return render_template("result.html", username = username, change = change, new_price=new_price)
+        if change == 0:
+            return
+        
+
+    else:
+        return redirect("google.com")
+        
+@app.route("/home", methods=['GET','POST'])
+def home():
+    return redirect("/")
