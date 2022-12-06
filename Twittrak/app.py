22e4c9ab2243680f71a04eb651d493b1167998cb
hugon-unez <63176618+hugon-unez@users.noreply.github.com>
2022-12-06 17:24:04 -0500

    Update app.py

--- Twittrak/app.py
+++ Twittrak/app.py
@@ -1,24 +1,14 @@
 # reused general implementation from finance in order to have a running front end
-import os
-from flask import Flask, flash, redirect, render_template, request, session, url_for
-from flask_session import Session
-from tempfile import mkdtemp
-from werkzeug.security import check_password_hash, generate_password_hash
+from flask import Flask, redirect, render_template, request
 from backend.stock_data import recent_price
 from backend.stock_prediction import predicted_change
-
 # short helper to format money from finance pset
 def usd(value):
     return f"${value:,.2f}"
-
 # Configure application
 app = Flask(__name__)
 app.jinja_env.filters["usd"] = usd
-
-
 # Configure session to use filesystem (instead of signed cookies)
-
-
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
@@ -26,17 +16,17 @@ def after_request(response):
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
-
-
 @app.route("/")
 def index():
     return render_template("index.html")
-
 @app.route("/correlate", methods=['GET','POST'])
 def correlate():
     if request.method == "POST":
         username = request.form.get("username")
         #check if valid username
+        for char in username:
+            if not char.isnumeric() and not char.isalpha():
+                return redirect("/")
         change = predicted_change(username)
         if change == "Invalid Input":
             return redirect("/")
@@ -47,14 +37,11 @@ def correlate():
             return render_template("result.html", username = username, change = change, new_price=new_price)
         if change < 0:
             change = abs(change)
-            return render_template("result.html", username = username, change = change, new_price=new_price)
+            return render_template("resultbad.html", username = username, change = change, new_price=new_price)
         if change == 0:
-            return
-        
-
+            return render_template("noresult.html", username = username, change = change, new_price=new_price)
     else:
-        return redirect("google.com")
-        
+        return redirect("/")
 @app.route("/home", methods=['GET','POST'])
 def home():
     return redirect("/")
