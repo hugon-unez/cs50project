694369c8191056181d29f22aa4216264277a5064
Roblockpower2 <112822194+Roblockpower2@users.noreply.github.com>
2022-12-06 16:38:36 -0500

    Update twitter_data.py

--- Twittrak/backend/twitter_data.py
+++ Twittrak/backend/twitter_data.py
@@ -1,36 +1,24 @@
-
-
 from datetime import datetime, timedelta
 #pytz to convert time zone
 import pytz
 import tweepy
-from backend.stock_data import stock_data 
+from data import stock_data
 
-def twitter_stuff(username):
+def twitter_stuff():
     # we decided to use a wrapper for twitter's api in order to easily import/use twitter data
 
     client = tweepy.Client("AAAAAAAAAAAAAAAAAAAAAI6BjwEAAAAAaSCf62jC8wxVlU0RphhCEvDxla0%3D0392CJtnYHJL1eA9TVoC6Tj84k347tiWMIXsPyHXehgUeni0sH")
 
     #make a placeholder for the user
-   
-    query = 'from:%s' %username
-    user = client.get_user(username=username)
-     # COME BACK AND CHECK IF VALID USERNAME
-    if user[0] == None:
-        print("Invalid Username")
-        return 44
+    query = 'from:potus'
     tweets = client.search_recent_tweets(query=query, max_results=100, tweet_fields=["created_at"])
 
-    if tweets[0] == None:
-        print("No tweets")
-        return 45
     #make sure to account for user having less than 50 tweets, or
 
-    # cleaning data for outside market time/days. this was one of the hardest parts of implementation due to the types
+    # cleaning data for outside market time/days.
     # type of created_at is datetime.datetime
 
     tweet_times = []
-    
 
     # in clean_time the tweet times are converted to est
     def clean_time(tweet):
@@ -45,5 +33,6 @@ def twitter_stuff(username):
         # if the tweet was after market closing set time to the last market close time
         twit = clean_time(tweet.created_at)
         tweet_times.append(str(twit))
+
     # at this stage, tweet_times should be cleaned and be ready to input to financial data
     return stock_data('SPY', tweet_times)
