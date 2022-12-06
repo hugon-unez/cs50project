5bddf2466efb2ed71bbf6acc09fcf9cd2c2c4cde
Roblockpower2 <112822194+Roblockpower2@users.noreply.github.com>
2022-12-05 21:31:37 -0500

    Update data.py

--- api/data.py
+++ api/data.py
@@ -17,3 +17,10 @@ def stock_data(ticker, tweet_times):
     # format for close price is data['Time Series (1min)'][time]['4. close']
     return table_of_stuff
     #print(float(data['Time Series (1min)'][time]['1. open']))
+def recent_price():
+   url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=SPY&interval=5min&outputsize=full&apikey=XK9UE7MB6UJJ1XUL'
+   r = requests.get(url)
+   data = r.json()
+   most_recent = data['Meta Data']['3. Last Refreshed']
+
+   return [float(data['Time Series (5min)'][most_recent]['1. open']), float(data['Time Series (5min)'][most_recent]['5. volume'])]
